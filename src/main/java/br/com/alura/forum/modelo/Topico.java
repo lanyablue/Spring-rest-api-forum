9a9854bc59fa73dd5ffea7a928ed582a8e4ef68c
lanyablue <barbaramy_@hotmail.com>
2022-07-09 18:15:53 -0300

    spring data JPA

--- src/main/java/br/com/alura/forum/modelo/Topico.java
+++ src/main/java/br/com/alura/forum/modelo/Topico.java
@@ -1,19 +1,33 @@
 package br.com.alura.forum.modelo;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Topico {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)  // auto_increment
 	private Long id;
 	private String titulo;
 	private String mensagem;
 	private LocalDateTime dataCriacao = LocalDateTime.now();
+
+	@Enumerated(EnumType.STRING)      // grave o nome da contante em vez da ordem
 	private StatusTopico status = StatusTopico.NAO_RESPONDIDO;
+
+	@ManyToOne
 	private Usuario autor;
+	@ManyToOne
 	private Curso curso;
+
+	@OneToMany(mappedBy = "topico")
 	private List<Resposta> respostas = new ArrayList<>();
+
+	public Topico() {
+	}
 
 	public Topico(String titulo, String mensagem, Curso curso) {
 		this.titulo = titulo;
