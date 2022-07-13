c401ef9acb2368c818c208589c3e53723d3b179a
lanyablue <barbaramy_@hotmail.com>
2022-07-13 09:55:17 -0300

    restringindo acesso aos endpoints

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,6 +1,10 @@
 package br.com.alura.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -10,6 +14,21 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
 
 
+    @Override // Configurações de autenticação:
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    }
 
+    @Override // Configurações de autorização
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin();
+    }
+
+    @Override // Configurações de recursos estaticos(js, css, imagens, etc)
+    public void configure(WebSecurity web) throws Exception {
+    }
 
 }
