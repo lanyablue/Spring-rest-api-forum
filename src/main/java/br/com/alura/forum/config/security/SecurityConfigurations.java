1376d04be047d7e306bb7b1bf5a5a9e9528a45cd
lanyablue <barbaramy_@hotmail.com>
2022-07-13 14:03:33 -0300

    security

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,15 +8,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity // habilitando a parte do security na aplicação
 @Configuration    // habilita a leitura das configurações de bin dentro da classe
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private AutenticacaoService autenticacaoService;
 
-
-    @Override // Configurações de autenticação:
+    @Override // Configurações de autenticação
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
     @Override // Configurações de autorização
@@ -30,5 +34,6 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     @Override // Configurações de recursos estaticos(js, css, imagens, etc)
     public void configure(WebSecurity web) throws Exception {
     }
+
 
 }
